2099daf7ecdd2811257d0e9baa85922be7ea6ac3
Nguyen Hai Trieu <trieunguyenbaby13@gmail.com>
2024-03-23 07:57:42 +0700

    feat: Cretae have file env for enviroment different

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ import express from 'express';
 import configViewEngine from './src/config/viewEngiene';
 import initRoutes from './src/router/router';
 import dotenv from 'dotenv';
+
 dotenv.config();
 const app = express();
 // config view engine
 configViewEngine(app);
 // config router
 initRoutes(app);
+// connectDB();
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, () => {
     console.log('<<<<<<< Serser is running >>>>>>>', PORT);
