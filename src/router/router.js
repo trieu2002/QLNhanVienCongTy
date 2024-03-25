34ffe9dc3c5ab0063a1bb232780ef0b1b3f99b86
Nguyen Hai Trieu <trieunguyenbaby13@gmail.com>
2024-03-25 07:54:53 +0700

    feat:Createn router module user"

--- src/router/router.js
+++ src/router/router.js
@@ -3,6 +3,8 @@ import { ValidationMiddleware } from '../middleware/validation';
 import { LoginSchema, RegisterSchema } from '../validation/auth/auth';
 import { AuthController } from '../controller/auth/auth.controller';
 import { checkAuthGuard, checkAuthGuardPermission } from '../middleware/jwt-guard';
+import UserController from '../controller/entities/user.controller';
+import { createUserSchema, isValidID } from '../validation/entities/user.validation';
 const router = express.Router();
 const version = ['v1', 'v2'];
 const initRoutes = (app) => {
@@ -20,6 +22,10 @@ const initRoutes = (app) => {
     /**
      * Module User
      */
+    router.get('/user/view', UserController.getListUsers);
+    router.get('/user/viewById/:userId', ValidationMiddleware(isValidID), UserController.getUserById)
+    router.post('/user/create', ValidationMiddleware(createUserSchema), UserController.createUser);
+    router.delete('/user/delete/:userId', ValidationMiddleware(isValidID), UserController.deleteUser)
     return app.use('/api/v1', router);
 };
 export default initRoutes;
