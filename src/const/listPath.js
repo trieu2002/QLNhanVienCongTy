be2d51a6508ece3ca91877334a60079329f1a415
Nguyen Hai Trieu <trieunguyenbaby13@gmail.com>
2024-03-28 22:36:30 +0700

    feat:'Define many  prefix router "

---
+++ src/const/listPath.js
@@ -0,0 +1,14 @@
+export const whiteList = Object.freeze([
+    "/",
+    "login",
+    "/register",
+    "/forgot-password",
+    "/reset-password",
+    "/profile",
+    "/products",
+    "/products/:id",
+    "/products/delete/:id",
+    "/product/update/:id"
+]);
+// cách viêt unit testing
+
